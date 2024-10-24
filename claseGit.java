be55eac734d2d8dee405ed475e1475888c74b6c5
marvines07 <marv.dev07@gmail.com>
2024-10-23 20:42:26 -1200

    modificado de mensaje y creación clase test

--- claseGit.java
+++ claseGit.java
@@ -1,33 +1,10 @@
 
-/**
- * Write a description of class claseGit here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class claseGit
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class claseGit
-     */
-    public claseGit()
+    public static void main()
     {
-        // initialise instance variables
-        x = 0;
+        System.out.println("hola Git");
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
+       
